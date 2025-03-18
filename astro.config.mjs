3dadef155e17fb6a715e2f426c69c532a63fbf8f
SrRalo <adreloa@gmail.com>
2025-03-18 14:01:08 -0500

    prueba700

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://vbartalis.github.io",
-  base: "/miti-oficial"
+  base: "/miti-oficial/"
 });
