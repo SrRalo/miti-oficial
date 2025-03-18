a01bc5ab013f0c0d49a2f26e4738e9d288359bbf
SrRalo <adreloa@gmail.com>
2025-03-18 14:06:13 -0500

    web

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://vbartalis.github.io",
+  site: "https://miti-oficial.vercel.app/",
   base: "/miti-oficial/"
 });
